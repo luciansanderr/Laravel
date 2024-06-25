09a289f65ba1327bcfae8475de7e0f8e4f89f2b5
lucian.sander <Sanderlu@icloud.com>
2024-06-24 23:22:53 -0300

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+Route::get('/contatos', [ContatoController::class, 'contato']);
