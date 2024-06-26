1032d7c7751e0172f6f164dc448ea33bff55da19
lucian.sander <Sanderlu@icloud.com>
2024-06-26 10:40:10 -0300

    Condições e interação com blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,6 +8,22 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index () {
-        return view('app.fornecedor.index');
+
+        $fornecedores = [
+            // 'Fornecedor 1',
+            // 'Fornecedor 2',
+            // 'Fornecedor 3',
+            // 'Fornecedor 4',
+            // 'Fornecedor 5',
+            // 'Fornecedor 6',
+            // 'Fornecedor 7',
+            // 'Fornecedor 8',
+            // 'Fornecedor 9',
+            // 'Fornecedor 10',
+            // 'Fornecedor 11',
+            // 'Fornecedor 12'
+        ];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,7 +1,19 @@
 <h1>Fornecedor</h1>
 {{-- Comentando, descartado--}}
 <?= 'Olá'?>
+<br>
 @php
     //comentando
     echo 'Estou aqui';
 @endphp
+<br>
+
+{{-- @dd($fornecedores) --}}
+
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores. Limite: 10</h3>
+@elseif (count($fornecedores) > 10)
+    <h3>Existem vários fornecedores. Mais de 10</h3>
+@else
+    <h3>Não existem fornecedores</h3>
+@endif
