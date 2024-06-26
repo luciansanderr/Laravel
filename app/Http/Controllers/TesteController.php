5088616935e37da8349b3dd9536e44199fd93f7f
lucian.sander <Sanderlu@icloud.com>
2024-06-26 09:21:42 -0300

    Passando parâmetros por array associativa para view

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste (int $n1, int $n2) {
-        echo $n1 + $n2;
+
+        $x = $n1 + $n2;
+
+        return view('site.teste', ['x' => $x, 'n1' => $n1, 'n2' => $n2]);
     }
 }
---
+++ resources/views/site/teste.blade.php
@@ -0,0 +1,2 @@
+<h1>Soma de Números</h1>
+A soma de {{$n1}} + {{$n2}} = {{$x}}
