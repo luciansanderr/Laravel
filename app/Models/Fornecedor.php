f3090f46a6e786b8fd3bb55c2633990249c80e27
lucian.sander <Sanderlu@icloud.com>
2024-07-15 18:26:04 -0300

    Utilizando o SoftDelete para acrescentar uma coluna delete_at para utilizar para fazer a remoção de registros, não deletando o registro de fato só preenchendo a coluna delete_at

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    use SoftDeletes;
     use HasFactory;
     //utilizado para direcionar para a tabela com o nome diferente (Model: Fornecedor Migration: Fornecedores (Fornecedors))
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'uf', 'email', 'site'];
-    
+
 }
