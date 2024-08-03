ded81f49144849e18674c1b51874a0b370232f78
lucian.sander <Sanderlu@icloud.com>
2024-08-02 22:36:57 -0300

    Validação de email

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -52,6 +52,7 @@ class ContatoController extends Controller
         $request->validate ([
             'nome' => 'required|min:3|max:10',
             'telefone' =>'required',
+            'email' => 'email',
             'motivo_contato' => 'required',
             'mensagem' => 'required',
         ]);
