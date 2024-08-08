b5be5be62d1d65ffa7977c55dec3b54e141255f4
lucian.sander <Sanderlu@icloud.com>
2024-08-07 21:17:51 -0300

    Validação de erro apresentação msm

--- resources/views/site/layouts/_components/formulario.blade.php
+++ resources/views/site/layouts/_components/formulario.blade.php
@@ -30,9 +30,11 @@
         <button type="submit" class="{{ $classe }}">ENVIAR</button>
     </form>
 </div>
-
-<div style="position: absolute; top: 0px; left: 0px; width: 100%; background: red">
-    <pre>
-        {{ print_r($errors) }}
-    </pre>
-</div>
+@if ($errors->any()) {
+    <div style="position: absolute; top: 0px; left: 0px; width: 100%; background: red">
+        <pre>
+            {{ print_r($errors) }}
+        </pre>
+    </div>
+}
+@endif
