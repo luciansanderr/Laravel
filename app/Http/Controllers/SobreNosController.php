7609065b564201e0d637abd83f180f3a850c7584
Lucian Sander <Sanderlu@icloud.com>
2024-08-20 16:08:07 -0300

    Chamada dos middle (resolver mais a frente)

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreNosController extends Controller
 {
+    public function __construct() {
+       $this->middleware(LogAcessoMiddleware::class);
+    }
     public function sobrenos () {
         return view('site.sobre-nos');
     }
