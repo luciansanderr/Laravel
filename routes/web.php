ceb3107395b882b21c527863b811bb289e3f6ac3
Lucian Sander <Sanderlu@icloud.com>
2024-08-21 22:06:12 -0300

    Chamando em um grupo de rota o middleware

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,10 @@ Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contat
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 Route::get('/login', function () {return 'login';})->name('site.login');
 //agrupando com prefix as rotas
-Route::prefix('/app')->group(function() {
-    Route::middleware('log-acesso', 'autenticacao')
-    ->get('/clientes', function () {return 'clientes';})
-    ->name('app.clientes');
-    Route::middleware('log-acesso', 'autenticacao')
-    ->get('/fornecedores', [FornecedorController::class, 'index'])
-    ->name('app.fornecedor');
-    Route::middleware('log-acesso', 'autenticacao')
-    ->get('/produtos', function() {return 'produtos';})
-    ->name('app.produtos');
+Route::middleware('log-acesso', 'autenticacao')->prefix('/app')->group(function() {
+    Route::get('/clientes', function () {return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::get('/produtos', function() {return 'produtos';})->name('app.produtos');
 });
 
 // Route::get('/contatos/{nome}/{sobrenome}/{apelido?}', function ($nome, $sobrenome, $apelido = 'Não foi inserido nada'){
