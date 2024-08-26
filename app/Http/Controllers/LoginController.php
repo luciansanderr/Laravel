85e0e182b152713b25098121ba4855e040c1cecf
Lucian Sander <Sanderlu@icloud.com>
2024-08-26 18:06:36 -0300

    Construindo regras de validações

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,25 @@ class LoginController extends Controller
         return view('site.login');
     }
 
-    public function autenticar() {
-        echo "Chegamos até aqui";
+    // public function autenticar() {
+    //     echo "Chegamos até aqui";
+    // }
+
+    public function autenticar(Request $request) {
+        // regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+        // as mensagens de feedback de validação
+        $feedback = [
+            'usuario.email' => 'O campo usuário(e-mail) é obrigatório',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+
+        // se não passar pelo validade será retornado para rota anterior
+        $request->validate($regras, $feedback);
+
+        print_r($request);
     }
 }
