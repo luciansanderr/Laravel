1ba1ea4dd49f629fab925c0c67a674b695278b27
Lucian Sander <Sanderlu@icloud.com>
2024-08-27 17:32:01 -0300

    Criação e instanciação da model para verificação de usuário

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -30,6 +31,28 @@ class LoginController extends Controller
         // se não passar pelo validade será retornado para rota anterior
         $request->validate($regras, $feedback);
 
-        print_r($request);
+        //recuperação de dados
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        echo "Usuário: $email | Senha: $password";
+        echo "<br>";
+
+        // iniciar o model
+        $user = new User();
+        // comparações passo o parâmetro que chega pleo request e depois comparo o que está no banco
+        $usuario = $user->where('email', $email)
+            ->where('password', $password)
+            ->get()
+            ->first();
+        // condições e validações com confirmação de usuário
+        if (isset($usuario->name)) {
+            echo "Usuário Logado";
+        } else {
+            echo "Usuário Incorreto";
+        }
+            // echo "<pre>";
+            // print_r($usuario);
+            // echo "</pre>";
     }
 }
