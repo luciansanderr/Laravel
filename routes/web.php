9e3c678066ec85ea4469e85b44ef25f0a8ee82a1
lucian.sander <Sanderlu@icloud.com>
2025-07-03 17:49:51 -0300

    Criando controller, model, e rotas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PedidoProdutoController;
+use App\Models\Pedido;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
@@ -13,6 +15,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\ProdutoDetalheController;
+use App\Http\Controllers\PedidoController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -31,7 +34,6 @@ Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login'
 Route::middleware('autenticacao:padrao')->prefix('/app')->group(function() {
     Route::get('/home', [HomeController::class, 'index'])->name('app.home');
     Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');
-    Route::get('/cliente', [ClienteController::class, 'index'])->name('app.cliente');
     Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
     Route::post('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
     Route::get('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
@@ -43,6 +45,10 @@ Route::middleware('autenticacao:padrao')->prefix('/app')->group(function() {
     Route::resource('produto', ProdutoController::class);
     //produto_detalhe
     Route::resource('produto-detalhe', ProdutoDetalheController::class);
+
+    Route::resource('cliente', ClienteController::class);
+    Route::resource('pedido', PedidoController::class);
+    Route::resource('pedido-produto', PedidoProdutoController::class);
 
 });
 
